706e3adbdcdb52e3f92bc02568687b4690c3ff9e
hossamalbdrani8-sketch <hossamalbdrani8@gmail.com>
2026-04-06 11:36:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const bot = new TelegramBot(TOKEN, {
 });
 
 // 🔑 API
-const API_KEY = "d75o0l1r01qk56kdfon0l1r01qk56kdfong";
+const API_KEY = "d75o0l1r01qk56kdfon0d75o0l1r01qk56kdfong";
 
 let chatId = null;
 let memory = {};
