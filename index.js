51c0065fa3b9978784e3e349754caccbf6341d60
hossamalbdrani8-sketch <hossamalbdrani8@gmail.com>
2026-09-06 13:37:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 // 🔑 TOKEN
-const TOKEN = 8652994768:AAHg_ABByrZdvlljJ1dQfs6LSmBl37XMPXk
+const TOKEN = "8652994768:AAHg_ABByrZdvlljJ1dQfs6LSmBl37XMPXk"
 
 // 💀 اتصال قوي ثابت
 const bot = new TelegramBot(TOKEN, {
